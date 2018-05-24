ba76eff61c1f547c7a2a71e72f8eab6876ec9458
Jake Lewis <jakeaustinlewis@gmail.com>
2018-05-24 15:58:13 -0500

    same materials, but with cleaned up code

--- dog.js
+++ dog.js
@@ -1,12 +1,14 @@
 let searchDogButton = document.getElementById('searchDogButton');
-let typeOfDog = document.getElementById('typeOfDog');
+let breed = document.getElementById('breed');
 let dogPic = document.getElementById('dogPic');
+let ranDogButton = document.getElementById('ranDogButton');
+
 
 searchDogButton.addEventListener('click', function (e) {
-    if (typeOfDog.type !== "") {
+    if (breed.type !== "") {
         e.preventDefault();
-        typeOfDog.value=typeOfDog.value.replace(/\s/g,'');
-        let link = `https://dog.ceo/api/breed/${typeOfDog.value}/images`;
+        breed.value = breed.value.replace(/\s/g, '');
+        let link = `https://dog.ceo/api/breed/${breed.value}/images`;
         console.log('click worked');
         fetch(link)
             .then(response => response.json())
@@ -16,5 +18,25 @@ searchDogButton.addEventListener('click', function (e) {
                 dogPic.setAttribute('src', randomDogPic);
             })
     }
-
 });
+
+ranDogButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    let link = `https://dog.ceo/api/breeds/image/random`;
+    console.log('click worked');
+    fetch(link)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let randomDogPic = data.message;
+            dogPic.setAttribute('src', randomDogPic);
+        })
+});
+
+
+
+
+
+
+
+
